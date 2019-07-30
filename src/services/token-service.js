116b27fda84aa8078bf6d0968d2f26881fc54a3b
Mantong Zhao <zhaomantong0203@gmail.com>
2019-07-30 00:17:01 -0700

    protect endpoint

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -16,6 +16,9 @@ const TokenService = {
   makeBasicAuthToken(userName, password) {
     return window.btoa(`${userName}:${password}`)
   },
+  saveAuthToken(token) {
+    window.localStorage.setItem(config.TOKEN_KEY, token)
+  },
 }
 
 export default TokenService
